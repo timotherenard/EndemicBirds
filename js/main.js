cc7c8e348e52ddad030502f9bd251ede151201fd
timotherenard <timo.renard@hotmail.fr>
2023-08-10 13:19:33 +0400

    initialization

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+function responsivemenu(){
+	if (document.getElementById("navigationbar").className=="passive"){
+	document.getElementById("navigationbar").className="active";
+	}
+	else{
+		document.getElementById("navigationbar").className="passive";
+	}
+}
+
+function menuoff(){
+	document.getElementById("navigationbar").className="passive";
+}
+
+
+function showProperties(element){
+  document.getElementById('imgDescription').innerHTML = element.alt;
+}
